843226d163d70a6689125a3ee3a6e50abdd6e95e
Alejandro Cruz <vidriloco@gmail.com>
2019-07-01 12:01:48 -0500

    Initial Commit

---
+++ Pokemoncito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemoncito
+//
+//  Created by Alejandro on 7/1/19.
+//  Copyright © 2019 Kalakmul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
